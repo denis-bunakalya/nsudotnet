e5890d434847db336f45b62a167a7c81a6159fdd
Denis Bunakalya <denis_bunakalya@mail.ru>
2016-04-17 21:11:08 +0600

    добавление в TaskScheduler

--- Bunakalya.Nsudotnet.TaskScheduler/TaskScheduler/TaskScheduler.cs
+++ Bunakalya.Nsudotnet.TaskScheduler/TaskScheduler/TaskScheduler.cs
@@ -20,9 +20,13 @@ namespace TaskScheduler
     {
         static void Main(string[] args)
         {
-            TaskScheduler.ScheduleDelayedJob(new PrintJob(), "hello", new TimeSpan(0, 0, 2));
-            TaskScheduler.SchedulePeriodicJob(new PrintJob(), "hi", new TimeSpan(0, 0, 1));
+            RegisteredWaitHandle handle1 = TaskScheduler.ScheduleDelayedJob(new PrintJob(), "hello", new TimeSpan(0, 0, 2));
+            RegisteredWaitHandle handle2 = TaskScheduler.SchedulePeriodicJob(new PrintJob(), "hi", new TimeSpan(0, 0, 1));
+
             Thread.Sleep(5000);
+
+            handle2.Unregister(null);
+            Thread.Sleep(3000);
         }
 
         static void Execute(Object state, bool b)
@@ -30,15 +34,15 @@ namespace TaskScheduler
             ((IJob)((object[])state)[0]).Execute(((object[])state)[1]);
         }
 
-        static void ScheduleDelayedJob(IJob job, object argument, TimeSpan delay)
+        static RegisteredWaitHandle ScheduleDelayedJob(IJob job, object argument, TimeSpan delay)
         {
-            ThreadPool.RegisterWaitForSingleObject(new EventWaitHandle(false, EventResetMode.AutoReset),
+            return ThreadPool.RegisterWaitForSingleObject(new EventWaitHandle(false, EventResetMode.AutoReset),
                 Execute, new[] { job, argument }, delay, true);
         }
 
-        static void SchedulePeriodicJob(IJob job, object argument, TimeSpan period)
+        static RegisteredWaitHandle SchedulePeriodicJob(IJob job, object argument, TimeSpan period)
         {
-            ThreadPool.RegisterWaitForSingleObject(new EventWaitHandle(false, EventResetMode.AutoReset),
+            return ThreadPool.RegisterWaitForSingleObject(new EventWaitHandle(false, EventResetMode.AutoReset),
                 Execute, new[] { job, argument }, period, false);
         }
     }
