22bccc5776e6a7648bb066b7c37d39822a4c5512
Denis Bunakalya <denis_bunakalya@mail.ru>
2016-04-18 10:25:40 +0600

    исправление по issue в TicTacToe

---
+++ Bunakalya.Nsudotnet.TicTacToe/TicTacToe/Model.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Model
+    {
+        public int[,] Field { get; private set; }
+
+        private int _player;
+        private int _lastCell;
+        public int Winner { get; private set; }
+
+        public Model()
+        {
+            Field = new int[10, 10];
+
+            _player = 1;
+            _lastCell = 0;
+            Winner = 0;
+        }
+
+        public void MakeMove(int inputField, int inputCell)
+        {
+            if (!((_lastCell == 0) || (Field[0, _lastCell] == 9) || (inputField == _lastCell)))
+            {
+                throw new Exception(String.Format("You must choose the field number {0} (the number of the last cell)", _lastCell));
+            }
+
+            if (Field[inputField, inputCell] != 0)
+            {
+                throw new Exception("You must choose a free cell");
+            }
+
+            Field[inputField, inputCell] = _player;
+            Field[0, inputField]++;
+
+            Field[inputField, inputCell] = _player;
+            Field[0, inputField]++;
+
+            if ((Field[inputField, 0] == 0) && CheckWinField(inputField))
+            {
+                Winner = CheckWinGame();
+            }
+
+            _player *= -1;
+            _lastCell = inputCell;
+        }
+
+        private bool CheckWinField(int inputField)
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                int horizontal = 0;
+                int vertical = 0;
+                int diagonal = 0;
+
+                for (int j = 1; j <= 3; j++)
+                {
+                    horizontal += Field[inputField, i * 3 + j];
+                    vertical += Field[inputField, i + 1 + 3 * (j - 1)];
+                    if (i == 1)
+                    {
+                        diagonal += Field[inputField, 1 + (j - 1) * 4];
+                    }
+                    else if (i == 2)
+                    {
+                        diagonal += Field[inputField, 3 + (j - 1) * 2];
+                    }
+                }
+                if ((horizontal == 3) || (vertical == 3) || (diagonal == 3))
+                {
+                    Field[inputField, 0] = 1;
+                    return true;
+                }
+                if ((horizontal == -3) || (vertical == -3) || (diagonal == -3))
+                {
+                    Field[inputField, 0] = -1;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private int CheckWinGame()
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                int horizontal = 0;
+                int vertical = 0;
+                int diagonal = 0;
+
+                for (int j = 1; j <= 3; j++)
+                {
+                    horizontal += Field[i * 3 + j, 0];
+                    vertical += Field[i + 1 + 3 * (j - 1), 0];
+                    if (i == 1)
+                    {
+                        diagonal += Field[1 + (j - 1) * 4, 0];
+                    }
+                    else if (i == 2)
+                    {
+                        diagonal += Field[3 + (j - 1) * 2, 0];
+                    }
+                }
+                if ((horizontal == 3) || (vertical == 3) || (diagonal == 3))
+                {
+                    return 1;
+                }
+                if ((horizontal == -3) || (vertical == -3) || (diagonal == -3))
+                {
+                    return -1;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ Bunakalya.Nsudotnet.TicTacToe/TicTacToe/View.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+
+namespace TicTacToe
+{
+    class View
+    {
+        private readonly Model _model;
+
+        public View(Model model)
+        {
+            _model = model;
+        }
+
+        public void Show()
+        {
+            while (true)
+            {
+                PrintField(_model.Field);
+
+                while (true)
+                {
+                    Console.WriteLine("Enter the number of field (1-9) and the number of cell (1-9) without spaces:");
+                    string input = Console.ReadLine();
+
+                    int inputField;
+                    int inputCell;
+
+                    if ((input.Length != 2)
+                        || !int.TryParse(input.Substring(0, 1), out inputField)
+                        || !int.TryParse(input.Substring(1, 1), out inputCell)
+                        || (inputField == 0) || (inputCell == 0))
+                    {
+                        continue;
+                    }
+                    try
+                    {
+                        _model.MakeMove(inputField, inputCell);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        continue;
+                    }
+                    break;
+                }
+                if (_model.Winner != 0)
+                {
+                    PrintField(_model.Field);
+                    Console.WriteLine("The player {0} has won", (_model.Winner == 1) ? 'X' : '0');
+                    return;
+                }
+            }
+        }
+
+        private static void PrintField(int[,] field)
+        {
+            for (int m = 0; m <= 6; m = m + 3)
+            {
+                string s = string.Format("{0}{1}{2}", 
+                    (field[m + 1, 0] == -1) ? "0000000" : (field[m + 1, 0] == 1) ? "XXXXXXX" : "       ",
+                    (field[m + 2, 0] == -1) ? "0000000" : (field[m + 2, 0] == 1) ? "XXXXXXX" : "       ",
+                    (field[m + 3, 0] == -1) ? "0000000" : (field[m + 3, 0] == 1) ? "XXXXXXX" : "       ");
+
+                Console.WriteLine(s);
+                for (int k = 0; k <= 6; k = k + 3)
+                {
+                    for (int i = 1 + m; i <= 3 + m; i++)
+                    {
+                        Console.Write((field[i, 0] == -1) ? '0' : (field[i, 0] == 1) ? 'X' : ' ');
+                        for (int j = 1 + k; j <= 3 + k; j++)
+                        {
+                            Console.Write("{0}{1}", (field[i, j] == -1) ? '0' : (field[i, j] == 1) ? 'X' : ' ', 
+                                (j == 3 + k) ? string.Empty : "|");
+                        }
+                        Console.Write((field[i, 0] == -1) ? '0' : (field[i, 0] == 1) ? 'X' : ' ');
+                    }
+                    Console.WriteLine();
+                    Console.Write((k == 6) ? string.Empty : string.Format("{0}-|-|-{0}{1}-|-|-{1}{2}-|-|-{2} \n", 
+                        s.ElementAt(0), s.ElementAt(7), s.ElementAt(14)));
+                }
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
